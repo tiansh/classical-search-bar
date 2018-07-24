625598de8c17b8a1cc7f188666c7d7b52f01cf95
田生 <tiansheng111@gmail.com>
2018-07-25 00:46:27 +1000

    Better user interface for popup.css
    support theme
    fix scroll issues when too many items
    support arrow keys

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,4 +1,4 @@
-; (function () {
+; (async function () {
 
   const callBackend = new Proxy({}, {
     get: (empty, method) => (...params) => (
@@ -10,83 +10,119 @@
     document.addEventListener('DOMContentLoaded', () => resolve());
   });
 
-  const searchProviderListPromise = callBackend.getList();
-  const currentSearchProviderPromise = callBackend.getDefault();
+  const [
+    searchProviderList,
+    defaultSearchProvider,
+  ] = await Promise.all([
+    callBackend.getList(),
+    callBackend.getDefault(),
+  ]);
 
-  Promise.all([domReady, searchProviderListPromise, currentSearchProviderPromise]).then(([, searchProviderList, defaultSearchProvider]) => {
-    const menu = document.getElementById('menu');
-    const menuItems = document.createDocumentFragment();
-    searchProviderList.forEach(({ id, name, favicon_url }) => {
-      /** @type {HTMLTemplateElement} */
-      const template = document.getElementById('menuitem');
-      const content = template.content;
-      const menuText = content.querySelector('.text');
-      menuText.textContent = name;
-      const menuIcon = content.querySelector('.icon img');
-      menuIcon.src = favicon_url;
-      const menuItem = content.querySelector('.panel-list-item');
-      menuItem.dataset.id = id;
-      if (id === defaultSearchProvider.id) {
-        menuItem.classList.add('current');
-      } else {
-        menuItem.classList.remove('current');
-      }
-      menuItems.appendChild(document.importNode(menuItem, true));
-    });
-    menu.appendChild(menuItems);
-    addEventListeners();
+  const menuItems = searchProviderList.map(({ id, name, favicon_url }) => {
+    /** @type {HTMLTemplateElement} */
+    const template = document.getElementById('menuitem');
+    const content = template.content;
+    const menuText = content.querySelector('.text');
+    menuText.textContent = name;
+    const menuIcon = content.querySelector('.icon img');
+    menuIcon.src = favicon_url;
+    const menuItem = content.querySelector('.menuitem');
+    menuItem.dataset.id = id;
+    if (id === defaultSearchProvider.id) {
+      menuItem.classList.add('current');
+      menuItem.setAttribute('aria-current', 'true');
+    } else {
+      menuItem.classList.remove('current');
+      menuItem.removeAttribute('aria-current');
+    }
+    return document.importNode(menuItem, true);
   });
+  document.getElementById('menu').append(...menuItems);
 
-  const addEventListeners = function () {
-    const eventMenuItem = event => {
-      const target = event.target;
-      const menuitem = target.closest && target.closest('.panel-list-item');
-      return menuitem;
-    };
-    const allMenuItems = Array.from(document.querySelectorAll('.panel-list-item'));
-    const currentMenuItem = () => {
-      return allMenuItems.find(item => item.classList.contains('current'));
-    };
-    const setCurrentMenuItem = current => {
-      allMenuItems.forEach(item => {
-        if (item === current) item.classList.add('current');
-        else item.classList.remove('current');
-      });
-    };
-    const choseMenuItem = item => {
-      const id = item && +item.dataset.id || item.dataset.id;
-      if (!id) return;
-      callBackend.setDefault(id).then(() => {
-        window.close();
-      });
-    };
-    document.addEventListener('click', event => {
-      const item = eventMenuItem(event);
-      choseMenuItem(item);
-    });
-    document.addEventListener('mousemove', event => {
-      const item = eventMenuItem(event);
-      if (item) setCurrentMenuItem(item);
-    });
-    document.addEventListener('keyup', event => {
-      const key = event.keyCode;
-      if ([38, 40].includes(key)) {
-        const current = currentMenuItem();
-        const index = allMenuItems.indexOf(current);
-        const next = index + (key === 38 ? -1 : 1);
-        const newIndex = next < 0 ?
-          allMenuItems.length - 1 :
-          next >= allMenuItems.length ? 0 : next;
-        setCurrentMenuItem(allMenuItems[newIndex]);
-      } else if (48 < key && key < 58) {
-        const index = key - 48 - 1;
-        if (index < allMenuItems.length) {
-          choseMenuItem(allMenuItems[index]);
-        }
-      } else if (key === 13) {
-        choseMenuItem(currentMenuItem());
+  const eventMenuItem = event => {
+    const target = event.target;
+    const menuitem = target.closest && target.closest('.menuitem');
+    return menuitem;
+  };
+  const currentMenuItem = () => {
+    return menuItems.find(item => item.classList.contains('current'));
+  };
+  const scrollCurrentIntoView = () => {
+    currentMenuItem().scrollIntoView({ behavior: 'instant', block: 'nearest' });
+  };
+  const setCurrentMenuItem = current => {
+    menuItems.forEach(item => {
+      if (item === current) {
+        item.classList.add('current');
+        item.setAttribute('aria-current', 'true');
+      } else {
+        item.classList.remove('current');
+        item.removeAttribute('aria-current');
       }
     });
   };
+  const choseMenuItem = item => {
+    const id = item && +item.dataset.id || item.dataset.id;
+    if (!id) return;
+    callBackend.setDefault(id).then(() => {
+      window.close();
+    });
+  };
+  document.addEventListener('click', event => {
+    if (event.button !== 0) return;
+    const item = eventMenuItem(event);
+    choseMenuItem(item);
+  });
+  document.addEventListener('mousemove', event => {
+    const item = eventMenuItem(event);
+    if (item) setCurrentMenuItem(item);
+  });
+  document.addEventListener('keydown', event => {
+    const key = event.keyCode;
+    if ([38, 40].includes(key)) {
+      const current = currentMenuItem();
+      const index = menuItems.indexOf(current);
+      const next = index + (key === 38 ? -1 : 1);
+      const newIndex = next < 0 ?
+        menuItems.length - 1 :
+        next >= menuItems.length ? 0 : next;
+      setCurrentMenuItem(menuItems[newIndex]);
+      scrollCurrentIntoView();
+    } else if (48 < key && key < 58) {
+      const index = key - 48 - 1;
+      if (index < menuItems.length) {
+        choseMenuItem(menuItems[index]);
+      }
+    } else if (key === 13 || key === 32) {
+      choseMenuItem(currentMenuItem());
+    } else return;
+    event.preventDefault();
+  });
+  document.addEventListener('contextmenu', event => {
+    event.preventDefault();
+  });
+  scrollCurrentIntoView();
+
+  const [theme, extensions] = await Promise.all([
+    browser.theme.getCurrent(),
+    browser.management.getAll(),
+  ]);
+  const currentTheme = extensions.find(extension => [
+    'default-theme@mozilla.org',
+    'firefox-compact-light@mozilla.org@personas.mozilla.org',
+    'firefox-compact-dark@mozilla.org@personas.mozilla.org',
+  ].includes(extension.id) && extension.enabled);
+  const rootStyle = document.documentElement.style;
+  if (currentTheme && currentTheme.id === 'firefox-compact-dark@mozilla.org@personas.mozilla.org') {
+    rootStyle.setProperty('--popup', '#4a4a4f');
+    rootStyle.setProperty('--popup-text', '#f9f9fa');
+  }
+  ['popup', 'popup_text'].forEach(key => {
+    const color = (theme.colors || {})[key];
+    if (!color) return;
+    const value = Array.isArray(color) ? `rgb(${color})` : color;
+    const cssKey = key.replace(/_/g, '-');
+    rootStyle.setProperty(`--${cssKey}`, value);
+  });
 
 }());
